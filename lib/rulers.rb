e7eb72b05721aa27b47d2439a162443db46038a4
Maximo Chalbaud <maxichalbaud@MacBook-Pro-de-Maximo.local>
2023-04-15 13:06:02 -0300

    deleting

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -5,8 +5,9 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`
+
       [
-        `echo debug > debug.txt`,
         200, {'Content-Type' => 'text/html'},
         ["Hello from Ruby on Rulers!"]
       ]
